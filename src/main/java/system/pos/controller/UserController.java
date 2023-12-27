c392715cbde8abdda06fc37d79585e2f02bae6d3
mccreed13 <a.lapushenko13@gmail.com>
2023-12-27 19:40:50 +0200

    Added lab 9 with report.

--- src/main/java/system/pos/controller/UserController.java
+++ src/main/java/system/pos/controller/UserController.java
@@ -1,4 +1,30 @@
 package system.pos.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import system.pos.entity.User;
+import system.pos.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return userService.listAll();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/users/{id}")
+    public Optional<User> getUsers(@PathVariable Long id){
+        return userService.getById(id);
+    }
 }
